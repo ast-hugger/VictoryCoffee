1a46a70a8024b6ba34d115b5e9046b332be93306
Vassili Bykov <newspeakbigot@gmail.com>
2016-10-18 09:51:54 -0700

    Attached copyright notices.

--- Newspeak/src/org/newspeaklanguage/compiler/NamingPolicy.java
+++ Newspeak/src/org/newspeaklanguage/compiler/NamingPolicy.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2016 Vassili Bykov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.newspeaklanguage.compiler;
 
 /**
@@ -44,7 +60,7 @@ public final class NamingPolicy {
   }
 
   /**
-   * Given a Newspeak methodName assumed to be a slot getter or setter, return the
+   * Given a Newspeak selector assumed to be a slot getter or setter, return the
    * name of the actual slot. The name is in the original unmangled form.
    */
   public static String slotForSelector(String selector) {
@@ -56,14 +72,14 @@ public final class NamingPolicy {
   }
   
   /**
-   * Given a Newspeak methodName, return a method name to be used for a method
-   * implementing that methodName.
+   * Given a Newspeak selector, return a method name to be used for a method
+   * implementing that selector.
    */
   public static String methodNameForSelector(String selector) {
     StringBuilder name = new StringBuilder(selector.length() + 5);
     name.append('$');
     if (Character.isLetter(selector.charAt(0))) {
-      // A unary or keyword methodName
+      // A unary or keyword selector
       int part = 0;
       for (String keyword : selector.split(":")) {
         if (part++ > 0) {
@@ -75,7 +91,7 @@ public final class NamingPolicy {
         name.append('$');
       }
     } else {
-      // A binary methodName
+      // A binary selector
       for (char c : selector.toCharArray()) {
         name.append('$');
         name.append(binarySelectorCharToToken(c));
@@ -109,7 +125,7 @@ public final class NamingPolicy {
       case '&':
         return "amp";
       default:
-        throw new IllegalArgumentException("Unrecognized binary methodName char: " + character);
+        throw new IllegalArgumentException("Unrecognized binary selector char: " + character);
     }
   }
 
