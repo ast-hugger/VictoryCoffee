1a64423d51f03b196206026155e2f09a440ca5fa
Vassili Bykov <newspeakbigot@gmail.com>
2016-10-21 23:09:34 -0700

    Added an Ant build file and a Runner class to use when everything is packaged as an application jar.

---
+++ Newspeak/src/org/newspeaklanguage/apps/Runner.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2016 Vassili Bykov.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.newspeaklanguage.apps;
+
+import org.newspeaklanguage.compiler.Compiler;
+import org.newspeaklanguage.runtime.ClassDefinition;
+import org.newspeaklanguage.runtime.NewspeakClassLoader;
+import org.newspeaklanguage.runtime.NsObject;
+import org.newspeaklanguage.runtime.ObjectFactory;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * This is used as the main app class when the whole thing is packaged up as a jar.
+ *
+ * @author Vassili Bykov <newspeakbigot@gmail.com>
+ */
+public class Runner {
+  @SuppressWarnings("unchecked")
+  public static void main(String[] args) {
+
+    if (args.length != 1) {
+      say("Need a name of a Newspeak file defining a class named App with a 0-argument method named main.");
+      return;
+    }
+    String source;
+    try {
+      source = readFile(args[0]);
+    } catch (IOException e) {
+      say("Error reading the file: " + e);
+      return;
+    }
+
+    say("Compiling...");
+    List<Compiler.Result> results = Compiler.compile(source);
+    say("Loading...");
+    NewspeakClassLoader classLoader = new NewspeakClassLoader(TryFibonacci.class.getClassLoader());
+    results.forEach(each
+        -> classLoader.addBytecode(each.implementationClassName(), each.bytecode()));
+    classLoader.dumpClassFiles();
+    Class<? extends NsObject> topClass;
+    try {
+      topClass = (Class<? extends NsObject>) classLoader.loadClass(results.get(0).implementationClassName());
+    } catch (ClassNotFoundException e) {
+      throw new IllegalStateException("failure loading the compiled Newspeak class");
+    }
+    say("Setting up...");
+    ClassDefinition classDef = ClassDefinition.create("App", topClass);
+    ObjectFactory factory = ObjectFactory.create(classDef, null);
+    Object module = factory.makeInstance();
+    say("Running...");
+    long start = System.nanoTime();
+    Object x = invoke(module, "$main");
+    long stop = System.nanoTime();
+    say("Result: " + x);
+    say("in " + ((stop - start) / 1_000_000) + "ms");
+    say("Done.");
+  }
+
+  private static void say(String message) {
+    System.out.println(message);
+  }
+
+  private static String readFile(String path)
+      throws IOException
+  {
+    byte[] encoded = Files.readAllBytes(Paths.get(path));
+    return new String(encoded, Charset.defaultCharset());
+  }
+
+  private static Object invoke(Object object, String methodName) {
+    try {
+      Method method = object.getClass().getMethod(methodName, int.class);
+      return method.invoke(object, 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+}
--- README.md
+++ README.md
@@ -1,10 +1,60 @@
 # Victory Coffee #
 
-An implementation of Newspeak on the JVM. No truffles, just pure invokedynamic.
+An implementation of Newspeak on the JVM. No truffles, just raw
+invokedynamic.
 
-This is a proof-of-concept prototype implementing a subset of Newspeak
-more or less as it existed at Cadence in late 2008. It has class
-nesting but not mixins. It does implement proper closures.
+This is a proof-of-concept prototype for the subset of Newspeak more
+or less as it existed at Cadence in late 2008. It has class nesting
+but not mixins. It does implement proper closures we did not have in 2008.
+The focus at this point is at the core compilation
+scheme. Runtime is rudimentary at best, just enough to run some basic
+test examples. The following are not implemented because they are
+trivial: subclassing, class initializers, slot initializers.
 
-This branch is the first cut of the implementation, with all Newspeak values
-represented as Java Objects, and all integers wrapped.
+The AllWrapped branch is the implementation approach where every
+Newspeak value is represented as a single JVM Object reference, with
+integers always wrapped. It is simple.
+
+The default branch is current work in progress aiming at using
+primitive ints for integers, by using parallel data paths. Each
+Newspeak value is represented as a pair of JVM values, an Object and
+an int. When the Object value is an UNDEFINED constant, the Newspeak
+value is represented by the int, otherwise it's represented by the
+object. The tricky part are message send return values. A
+straightforward implementation with calls and returns would still
+require wrapping. The only way to avoid that seems to be the CPS. Or
+perhaps some clever type feedback optimization I haven't figured out
+yet.
+
+## Prerequisites, Building, and Running ##
+
+The project can be built using the included Ant build file.
+
+This repository is also an IntelliJ project and can be open directly
+in IntelliJ. The `$LIBRARIES$` variable will need to be set to
+point at the directory with third party frameworks (see below).
+IntelliJ is supposed to prompt for that the first time you open
+the project.
+
+The core module (Newspeak) depends on ANTLR4 and ASM, which need
+to be downloaded separately. The current setup expects a directory
+somewhere containing the following:
+
+  * antlr-4.5.3-complete.jar
+  * asm-5.1/lib/asm-5.1.jar
+
+In the `build.xml` file, fix the `{lib.dir}` property to point at that
+directory.
+
+Run
+
+    ant jar
+
+to compile everything and build a compiler app. Then run
+
+    java -jar build/VictoryCoffee.jar fib.ns
+
+to compile and run the Fibonacci example. Which is about as far as the included runtime
+library will take you.
+
+
